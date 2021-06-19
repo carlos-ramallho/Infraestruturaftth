d75ec7c8658b3888e9e6257b2877a4322a27fc56
ftth <carlosramalho04@hotmail.com>
2021-06-19 14:32:43 -0300

    :recycle: alterações no login

--- js/login.js
+++ js/login.js
@@ -5,16 +5,14 @@ function logar() {
 
     if (email.value == "admin@admin.com" && senha.value == "1234") {
         localStorage.setItem("acesso", true);
+        alert("Usuário Autenticado!");
         window.location.href = "index.html";
-    } else {
-        alert("Usuário ou senha inválidos!");
-    }
-
-    if (email.value == "carlosramalho04@hotmail.com" && senha.value == "1003702") {
+    } else if (email.value == "carlosramalho04@hotmail.com" && senha.value == "1003702") {
         localStorage.setItem("acesso", true);
         alert("Usuário Autenticado!");
         window.location.href = "index.html";
     } else {
+        localStorage.setItem("acesso", false);
         alert("Usuário ou senha inválidos!");
     }
 
--- js/login1.js
+++
@@ -1,8 +0,0 @@
-var logado = false;
-if (localStorage.getItem("acesso") == "true") {
-    Logado == true;
-}
-if (logado != true) {
-    alert("Você não está altenticado!");
-    window.location.href = "login.html";
-}
